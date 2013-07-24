d333f8f831518188f476f0a3cedd3ff6aba83b3d
Logan <git@loganmoseley.com>
2013-07-24 11:46:18 -0400

    Add new item view controller

---
+++ Packer/LMNewItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  LMNewItemViewController.h
+//  Packer
+//
+//  Created by Logan Moseley on 7/24/13.
+//  Copyright (c) 2013 Logan Moseley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LMNewItemViewController : UIViewController
+
+@end
--- Packer/LMViewController.m
+++ Packer/LMViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "LMViewController.h"
+#import "LMItemDetailsViewController.h"
+#import "LMNewItemViewController.h"
 
 @interface LMViewController ()
 
@@ -66,56 +68,20 @@
     return cell;
 }
 
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
- {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
- }
- else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
- {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
-
 #pragma mark - Table view delegate
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
+    if ([segue.identifier isEqualToString:@"Details"])
+    {
+        LMItemDetailsViewController *detailsController = segue.destinationViewController;
+        NSLog(@"prepareForSegue to %@", [detailsController class]);
+    }
+    else if ([segue.identifier isEqualToString:@"New"])
+    {
+        LMNewItemViewController *newController = segue.destinationViewController;
+        NSLog(@"prepareForSegue to %@", [newController class]);
+    }
 }
 
 @end
